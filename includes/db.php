1dec67784d7f1664fa5ecb9c61a8e732c8f510f7
komolika07 <167891471+komolika07@users.noreply.github.com>
2025-06-16 20:26:02 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,1 +1,14 @@
+<?php
+$host = "localhost";
+$username = "Your username";
+$password = "Your password";
+$dbname = "your database name";
 
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} else {
+    // echo "Database connected successfully!";
+}
+?>
