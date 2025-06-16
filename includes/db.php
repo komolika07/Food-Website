16d7f63e916c982d3d6a4aac2e3cd7890d3a1563
komolika07 <167891471+komolika07@users.noreply.github.com>
2025-06-16 20:23:00 +0530

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -1,12 +1,1 @@
-<?php
-$host = "localhost";
-$username = "root";
-$password = "";
-$dbname = "food_delivery";
 
-$conn = new mysqli($host, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-?>
